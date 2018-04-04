20028c4937edf00212b628608af9cdb55833030b
bazingagin <bazinga931212@gmail.com>
2018-04-03 23:01:01 -0400

    fixed bug

--- pr1.py
+++ pr1.py
@@ -1,8 +1,10 @@
+# Zhiying Jiang
+# Tianyi Zhang
+# Angela Su
+
 import sys
 import Queue
 import os
-
-
 
 class Process:
 	def __init__(self, state, proc_id, arrival_time, cpu_burst_time, num_bursts, io_time):
@@ -31,6 +33,7 @@ class Process:
 		self.burst_time = 0
 		self.running_time = 0
 		self.first = True
+		self.last_added = 0
 	def __cmp__(self, other):
 		if self.cpu_burst_time > other.cpu_burst_time:
 			return 1
@@ -116,6 +119,8 @@ def main(argv):
 				proc = Process('READY', proc_id, arrival_time, cpu_burst_time, num_bursts, io_time)
 				queue.put(proc)
 				rr_queue.append(proc)
+		# FCFS algorithm
+		FCFS(out,rr_queue)
 		# RR algorithm
 		RR(out,rr_queue,rr_add, min_arrival)
 	except ValueError as e:
@@ -129,6 +134,127 @@ def convert_str(l):
 		res+=' '+e.id
 	res+=']'
 	return res
+
+
+def printQueue(queue):
+	if(queue == []):
+		str = "[Q <empty>]"
+	else:
+		str = "[Q"
+		for q in queue:
+			str += " " + q.id
+		str += "]"
+	return str
+
+def FCFS(f,queue, t_cs = 8):
+	time = 0
+	print "time 0ms: Simulator started for FCFS [Q <empty>]"
+	num_process = len(queue)
+	ready_queue = []
+	context_switch = False
+	using_cpu = -1
+	num_preemption = 0
+	num_context_switch = 0
+	num_burst = 0 
+	switch_start = 0
+	switch_end = 0
+	stop_cpu_time = 0	
+	slice_now = 0
+	while True:
+		io = -1
+		for i in range(num_process):
+			if queue[i].io:
+				io = i
+		if using_cpu<0 and io<0 and time > sys.maxint and ready_queue==[]:
+			break
+		elif using_cpu<0 and ready_queue!=[] and time >= sys.maxint+t_cs/2:
+			context_switch = True
+			if switch_start == 0:
+				switch_start = time-1
+		if ready_queue!=[]:
+			if context_switch and (time == sys.maxint+t_cs/2 or (time-switch_start) == t_cs):
+				num_context_switch+=1
+				process = ready_queue.pop(0)
+				using_cpu = queue.index(process)
+				print "time %dms: Process %s started using the CPU %s"%(time, queue[using_cpu].id, printQueue(ready_queue))
+				context_switch = False
+				queue[using_cpu].update_wait_time(queue[using_cpu].single_wait_time)
+				queue[using_cpu].update_turnaround_time(queue[using_cpu].single_wait_time)
+				queue[using_cpu].cpu_start_time = time
+				queue[using_cpu].single_wait_time = 0
+				slice_now = 0
+				queue[using_cpu].wait_time -= t_cs/2
+				switch_start = 0		
+		p = using_cpu
+		incomplete = True
+		if using_cpu>=0:
+			if queue[p].running_time-queue[p].cpu_start_time+time == queue[p].get_cpu_burst_time():
+				incomplete = False
+				stop_cpu_time = time
+				num_burst += 1
+				queue[p].update_turnaround_time(queue[p].get_cpu_burst_time()+t_cs/2)
+				queue[p].update_burst_time()
+				queue[p].update_num_bursts()
+				queue[p].cpu_start_time = 0
+				queue[p].running_time = 0
+				using_cpu = -1
+				if queue[p].num_bursts == 0:
+					print "time %dms: Process %s terminated %s"%(time, queue[p].id, printQueue(ready_queue))
+				else:
+					if queue[p].num_bursts > 1:
+						print "time %dms: Process %s completed a CPU burst; %d bursts to go %s"%(time, queue[p].id, queue[p].num_bursts, printQueue(ready_queue))
+					else:
+						print "time %dms: Process %s completed a CPU burst; %d burst to go %s"%(time, queue[p].id, queue[p].num_bursts, printQueue(ready_queue))
+					if queue[p].io_time>0:
+						queue[p].io = True
+						queue[p].io_finish_time = time + queue[p].io_time + t_cs/2
+						print "time %dms: Process %s switching out of CPU; will block on I/O until time %dms %s"%(time, queue[p].id, queue[p].io_finish_time,printQueue(ready_queue))
+					else:
+						ready_queue.append(queue[p])
+		for i in range(num_process):
+			if queue[i].io and time==queue[i].io_finish_time:
+				queue[i].io_finish_time = 0
+				if ready_queue==[] and queue[i].num_bursts>0 and using_cpu==-1:
+					context_switch=True
+					if time - stop_cpu_time <= t_cs/2:
+						switch_start = time
+					else:
+						switch_start = time - t_cs/2
+				ready_queue.append(queue[i])
+				print "time %dms: Process %s completed I/O; added to ready queue %s"%(time,queue[i].id,printQueue(ready_queue))
+				queue[i].io = False
+		for k in range(num_process):
+			if queue[k].arrival_time<=time and queue[k].first:
+				p = queue[k]
+				ready_queue.append(queue[k])
+				queue[k].single_wait_time=0
+				queue[k].first=False
+				print "time %dms: Process %s arrived and added to ready queue %s"%(p.arrival_time,p.id,printQueue(ready_queue))
+		for j in range(num_process):
+			if using_cpu != j and (not queue[j].io):
+				queue[j].single_wait_time+=1
+		time+=1
+		if ready_queue==[] and context_switch:
+			context_switch = False
+			switch_start = time
+	print "time %dms: Simulator ended for FCFS"%(time+t_cs/2-1)
+	wait_time = 0
+	burst_time = 0
+	turnaround_time = 0
+	for i in range(num_process):
+		wait_time += queue[i].wait_time
+		burst_time += queue[i].burst_time
+		turnaround_time += queue[i].turnaround_time
+	wait_time -= num_preemption*(t_cs/2)
+	if num_burst!=0:
+		wait_time /= float(num_burst)
+		burst_time /= float(num_burst)
+		turnaround_time /= float(num_burst)
+	else:
+		wait_time /= float(num_process)
+		burst_time /= float(num_process)
+		turnaround_time /= float(num_process)
+	write_result(f,'FCFS',burst_time,wait_time,turnaround_time,num_context_switch,num_preemption)
 
 def RR(f,queue,rr_add,min_arrival,t_slice=80, t_cs = 8):
 	time = 0
@@ -145,6 +271,8 @@ def RR(f,queue,rr_add,min_arrival,t_slice=80, t_cs = 8):
 	switch_end = 0
 	stop_cpu_time = 0	
 	slice_now = 0
+
+	wait_for_switch = 0
 	while True:
 		io = -1
 		for i in range(num_process):
@@ -157,9 +285,12 @@ def RR(f,queue,rr_add,min_arrival,t_slice=80, t_cs = 8):
 			# need improve 
 			if switch_start == 0:
 				switch_start = time-1
+		# check if a process finishes context switch
 		if ready_queue!=[]:
 			if context_switch and (time == min_arrival+t_cs/2 or (time-switch_start) == t_cs):
 				num_context_switch+=1
+				if rr_add!="BEGINNING":
+					ready_queue = sorted(ready_queue,key=lambda x: x.last_added)
 				process = ready_queue.pop(0)
 				using_cpu = queue.index(process)
 				if queue[using_cpu].running_time>0:
@@ -168,12 +299,14 @@ def RR(f,queue,rr_add,min_arrival,t_slice=80, t_cs = 8):
 					print "time %dms: Process %s started using the CPU %s"%(time, queue[using_cpu].id, convert_str(ready_queue))
 				context_switch = False
 
+				#print "before: process",queue[using_cpu].id,"has wait_time",queue[using_cpu].wait_time,"at time %dms"%time
 				queue[using_cpu].update_wait_time(queue[using_cpu].single_wait_time)
 				queue[using_cpu].update_turnaround_time(queue[using_cpu].single_wait_time)
 				queue[using_cpu].cpu_start_time = time
 				queue[using_cpu].single_wait_time = 0
 				slice_now = 0
 				queue[using_cpu].wait_time -= t_cs/2
+				#print "after: process",queue[using_cpu].id,"has wait_time",queue[using_cpu].wait_time,"at time %dms"%time
 				switch_start = 0
 				# process.cpu_start_time = time
 				# process.update_wait_time()
@@ -212,9 +345,7 @@ def RR(f,queue,rr_add,min_arrival,t_slice=80, t_cs = 8):
 						print "time %dms: Process %s switching out of CPU; will block on I/O until time %dms %s"%(time, queue[p].id, queue[p].io_finish_time,convert_str(ready_queue))
 					else:
 						#put back
-						# if rr_add=="BEGINNING":
-						# 	ready_queue.insert(0,queue[p])
-						# else:
+						queue[p].last_added = time
 						ready_queue.append(queue[p])
 
 			#check if t_slice finished
@@ -227,6 +358,9 @@ def RR(f,queue,rr_add,min_arrival,t_slice=80, t_cs = 8):
 						# if rr_add=="BEGINNING":
 						# 	ready_queue.insert(0,queue[p])
 						# else:
+
+						wait_for_switch = queue[p]
+						queue[p].last_added = time+t_cs/2
 						ready_queue.append(queue[p])
 						num_preemption+=1
 						using_cpu=-1
@@ -249,6 +383,7 @@ def RR(f,queue,rr_add,min_arrival,t_slice=80, t_cs = 8):
 						switch_start = time
 					else:
 						switch_start = time - t_cs/2
+				queue[i].last_added = time
 				if rr_add=="BEGINNING":
 					ready_queue.insert(0,queue[i])
 				else:
@@ -259,6 +394,7 @@ def RR(f,queue,rr_add,min_arrival,t_slice=80, t_cs = 8):
 		for k in range(num_process):
 			if queue[k].arrival_time<=time and queue[k].first:
 				p = queue[k]
+				queue[k].last_added = time
 				if rr_add=="BEGINNING":
 					ready_queue.insert(0,queue[k])
 				else:
@@ -292,6 +428,8 @@ def RR(f,queue,rr_add,min_arrival,t_slice=80, t_cs = 8):
 		wait_time /= float(num_process)
 		burst_time /= float(num_process)
 		turnaround_time /= float(num_process)
+	# print "wait time is ", wait_time
+	# print "turnaround time is ",turnaround_time
 	write_result(f,'RR',burst_time,wait_time,turnaround_time,num_context_switch,num_preemption)
 
 
